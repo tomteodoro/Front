ba6139c2be862ce0e6482ccc098754b6f5ae2daf
Tom Teodoro <wellintoncteodoro@gmail.com>
2026-03-03 23:41:42 -0300

    ajustes atividades

--- JavaScript/DOM/03032026/atividades.js
+++ JavaScript/DOM/03032026/atividades.js
@@ -28,3 +28,51 @@ texto.addEventListener("keyup", function() {
   let quantidade = texto.value.length;
   contador.textContent = "Caracteres digitados: " + quantidade;
 });
+
+// Atividade 4
+let botao = document.querySelector("#botao");
+let resultado = document.querySelector("#resultado");
+
+let contadorCliques = 0;
+
+botao.addEventListener("click", function() {
+  contadorCliques++;
+  resultado.textContent = "Cliques: " + contadorCliques;
+});
+
+// Atividade 5 e 6 + Extra
+let input = document.querySelector("#item");
+let botaoAdicionar = document.querySelector("#adicionar");
+let lista = document.querySelector("#lista");
+
+botaoAdicionar.addEventListener("click", function() {
+  if (input.value !== "") {
+    let novoItem = document.createElement("li");
+    novoItem.textContent = input.value;
+    lista.appendChild(novoItem);
+    input.value = "";
+
+    // novoItem.addEventListener("click", function() {
+    // if (confirm("Deseja remover este item?")) {
+    //   novoItem.remove();
+    // }
+    // });
+  }
+});
+
+// Desafio HARD
+let nome = document.querySelector("#nome");
+let enviar = document.querySelector("#enviar");
+let feedback = document.querySelector("#feedback");
+
+enviar.addEventListener("click", function() {
+
+  if (nome.value.trim() === "") {
+    feedback.textContent = "O campo nome é obrigatório!";
+    feedback.style.color = "red";
+  } else {
+    feedback.textContent = "Nome enviado com sucesso!";
+    feedback.style.color = "green";
+  }
+
+});
