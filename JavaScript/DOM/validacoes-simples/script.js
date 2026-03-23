44e86c89d31af92401d0a715b68aed480f31c6ea
Tom Teodoro <wellintoncteodoro@gmail.com>
2026-03-22 22:55:18 -0300

    ajustes regex

--- JavaScript/DOM/validacoes-simples/script.js
+++ JavaScript/DOM/validacoes-simples/script.js
@@ -1,35 +1,58 @@
 // E-MAIL
 let email = document.querySelector("#email");
-let validar = document.querySelector("#validar");
-let mensagem = document.querySelector("#mensagem");
-
-validar.addEventListener("click", function () {
-
-    if (!email.value.includes("@") || !email.value.includes(".")) {
-        mensagem.textContent = "E-mail inválido.";
-        mensagem.style.color = "red";
-    } else {
-        mensagem.textContent = "E-mail válido!";
-        mensagem.style.color = "green";
-    }
-
-});
+let msgEmail = document.querySelector("#msgEmail");
 
 // SENHA
 let senha = document.querySelector("#senha");
-let feedback = document.querySelector("#feedback");
+let msgSenha = document.querySelector("#msgSenha");
+let forcaSenha = "";
+
+// VALIDAR e ENVIO
+let validar = document.querySelector("#validar");
+let msgEnvio = document.querySelector("#msgEnvio");
 
 senha.addEventListener("keyup", function () {
 
     if (senha.value.length < 6) {
-        feedback.textContent = "Senha fraca";
-        feedback.style.color = "red";
-    // } else if (senha.value.length >= 6 && senha.value.length < 10) {
-    //     feedback.textContent = "Senha aceitável";
-    //     feedback.style.color = "orange";
+        msgSenha.textContent = "Senha fraca...";
+        msgSenha.style.color = "red";
+        forcaSenha = "fraca";
+    } else if (senha.value.length <= 10) {
+        msgSenha.textContent = "Senha aceitável.";
+        msgSenha.style.color = "orange";
+        forcaSenha = "aceitavel";
     } else {
-        feedback.textContent = "Senha aceitável";
-        feedback.style.color = "orange";
+        msgSenha.textContent = "Senha forte!";
+        msgSenha.style.color = "green";
+        forcaSenha = "forte";
+    }
+
+});
+
+validar.addEventListener("click", function () {
+
+    msgEnvio.textContent = "";
+
+    if (!email.value.includes("@") || !email.value.includes(".")) {
+        msgEmail.textContent = "E-mail inválido. Exemplo: texto@email.com";
+        msgEmail.style.color = "red";
+        return;
+    } else {
+        msgEmail.textContent = "E-mail válido!";
+        msgEmail.style.color = "green";
+    }
+
+    if (senha.value === "") {
+        msgSenha.textContent = "Preencha esse campo.";
+        msgSenha.style.color = "red";
+        return;
+    } else if (forcaSenha === "aceitavel" || forcaSenha === "forte") {
+        msgEnvio.textContent = "E-mail e senha validados com sucesso!";
+        msgEnvio.style.color = "green";
+        email.value = "";
+        msgEmail.textContent = "";
+        senha.value = "";
+        msgSenha.textContent = "";
     }
 
 });
